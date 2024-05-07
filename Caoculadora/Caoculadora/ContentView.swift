e8ed9b699bb8aaa9dca05b1e09d5d99951bb1426
Carol Costa <anacarolalvescosta@gmail.com>
2024-05-07 16:10:36 -0300

    adiciona arquivos do projeto

---
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caoculadora
+//
+//  Created by Ana Carolyne Alves Costa on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "pawprint")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
