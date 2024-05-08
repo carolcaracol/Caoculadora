9887b6b06c99953deec102c7103e4db7c578db25
Carol Costa <anacarolalvescosta@gmail.com>
2024-05-08 18:00:56 -0300

    adiciona imagem e botão de calcular

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,10 +11,12 @@ struct ContentView: View {
     
     @State var years: Int? = nil
     @State var months: Int? = nil
+    @State var result: Int?
     
     var body: some View {
         VStack(alignment: .leading) {
             Text("Qual a idade do seu cão")
+                .font(.system(size: 24))
             
             Text("Anos")
             TextField(
@@ -26,6 +28,25 @@ struct ContentView: View {
             TextField("e quantos meses alem disso ele tem?",
                       value: $months,
                       format: .number)
+            Text("porte")
+            // aq vai ficar o segmented control
+
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight:150)
+                    .frame(maxWidth: .infinity)
+                    .shadow(radius: 20)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack{
+                    Color.purple
+                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        .foregroundStyle(.white)
+                        
+                }
+            })
+            .cornerRadius(10)
+            .frame(height: 50)
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
